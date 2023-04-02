6a7f7535805c322574a9ceb274f53f119d2b1a23
Jeff Gunter <gunter.jeffm@gmail.com>
2023-04-02 14:26:10 -0400

    init

---
+++ recommender/Models/Query.cs
@@ -0,0 +1,6 @@
+namespace recommender.Models;
+
+public class Query
+{
+    public string? Name { get; set; }
+}
---
+++ recommender/Pages/Index.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using recommender.Models;
+
+namespace recommender.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public Query? Query { get; set; }
+
+    public void OnGet()
+    {
+
+    }
+}
